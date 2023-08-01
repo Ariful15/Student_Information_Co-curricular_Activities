347947914a31ccd42d751bf094dfad478ddcba35
Shaiekh Zayed Bin Hasan <zayed7h2@gmail.com>
2023-08-02 04:15:49 +0600

    Updated welcome screen with centered ASCII art and \xB2 border

--- Headers/welcome.h
+++ Headers/welcome.h
@@ -2,8 +2,10 @@
 #define WELCOME_H
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 void welcome(void);
+void print_centered(char *text);
 
 #endif /* WELCOME_H */
--- Sources/welcome.c
+++ Sources/welcome.c
@@ -3,9 +3,52 @@
 void welcome(void)
 {
     system("cls");
-    printf("\n\n\n\t\t\t\t\tWELCOME\n\t\t\t\t\t  TO\n");
-    printf("\t\tINFORMATION SYSTEM OF STUDENT CO CURRICULAR ACTIVITIES\n\n");
-    printf("\t\tEntering Main Menu in 3 seconds");
-    usleep(10000000);
+    printf("\n");
+
+    int i = 103;
+    while (i--)
+    {
+        printf("\xB2");
+    }
+
+    printf("\n");
+    print_centered("                                                                                         ");
+    print_centered("\\    / _  |  _  _  ._ _   _  ");
+    print_centered(" \\/\\/ (/_ | (_ (_) | | | (/_ ");
+    print_centered("                                                                                         ");
+    print_centered(" .     ");
+    print_centered("-|-.-. ");
+    print_centered(" '-`-' ");
+    print_centered("                                                                                         ");
+    print_centered(" __        __                                                                __            ");
+    print_centered("/   _  __ /      _ _. _   | _  _ /\\  _|_.  .|_  |\\/| _  _  _  _  _ _  _ _ |_(_    _|_ _ _  ");
+    print_centered("\\__(_)    \\__|_|| | |(_|_||(_|| /--\\(_|_|\\/||_\\/|  |(_|| )(_|(_)(-|||(-| )|___)\\/_)|_(-||| ");
+    print_centered("                                              /              _/                /           ");
+
+    i = 103;
+    while (i--)
+    {
+        printf("\xB2");
+    }
+
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+
+    printf("                                    Entering Main Menu in 3 seconds\r");
+    fflush(stdout);
+    usleep(1000000);
+    printf("                                    Entering Main Menu in 2 seconds\r");
+    fflush(stdout);
+    usleep(1000000);
+    printf("                                    Entering Main Menu in 1 second ");
+    usleep(1000000);
     system("cls");
+}
+
+void print_centered(char *text)
+{
+    int padding = (100 - strlen(text)) / 2;
+    printf("\xB2\xB2%*s%s%*s\xB2\xB2\n", padding, "", text, padding, "");
 }
